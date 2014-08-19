01cb1945297788b7dd3049da632b32469a26a664
lgc <liguangcheng@viigaa.com>
2014-08-19 09:36:07 +0800

    样式调整

---
+++ public/js/lrtk.js
@@ -0,0 +1,39 @@
+// 懒人图库 搜集整理 www.lanrentuku.com
+
+(function ($) {
+	$(function () {
+		nav(); //顶部导航
+		sideSlider(); //左侧竖导航
+	});
+	
+	function nav() {
+		var $liCur = $(".nav-box>ul>li.cur"),
+			curP = $liCur.position().left,
+			curW = $liCur.outerWidth(true),
+			$slider = $(".nav-line"),
+			$targetEle = $(".nav-box>ul>li:not('.last')"),
+			$navBox = $(".nav-box");
+		$slider.stop(true, true).animate({
+			"left" : curP,
+			"width" : curW
+		});
+		$targetEle.mouseenter(function () {
+			var $_parent = $(this);//.parent(),
+			_width = $_parent.outerWidth(true),
+			posL = $_parent.position().left;
+			$slider.stop(true, true).animate({
+				"left" : posL,
+				"width" : _width
+			}, "fast");
+		});
+		$navBox.mouseleave(function (cur, wid) {
+			cur = curP;
+			wid = curW;
+			$slider.stop(true, true).animate({
+				"left" : cur,
+				"width" : wid
+			}, "fast");
+		});
+	};
+	
+})(jQuery);
